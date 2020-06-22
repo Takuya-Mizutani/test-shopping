b0f006318bd8a855ee01d840300b970dc1ccf437
Takuya Mizutani <Takuya-Mizutani@users.noreply.github.com>
2020-06-22 18:45:57 +0900

    Second change

--- myshop/shopping/models.py
+++ myshop/shopping/models.py
@@ -15,7 +15,7 @@ class Book(models.Model):
 
 class BuyingHistory(models.Model):
     book = models.ForeignKey(Book, verbose_name='購入書籍', on_delete=models.PROTECT)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, varbose_name='購入ユーザ', on_delete=models.PROTECT)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='購入ユーザ', on_delete=models.PROTECT)
     is_sended = models.BooleanField('発送フラグ', default=False)
     stripe_id = models.CharField('タイトル', max_length=200)
     created_at = models.DateTimeField('日付', default=timezone.now)
