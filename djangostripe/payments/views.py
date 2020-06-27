a0c8e58655ce682ea1e9fd1cc7e07d20076c052f
Takuya Mizutani <Takuya-Mizutani@users.noreply.github.com>
2020-06-27 11:42:52 +0900

    llllllllllllll

--- djangostripe/payments/views.py
+++ djangostripe/payments/views.py
@@ -19,24 +19,33 @@ def stripe_config(request):
 
 @csrf_exempt
 def create_checkout_session(request):
-	if request.method == 'GET':
-		domain_url = 'http://localhost:8000/'
-		stripe.api_key = settings.STRIPE_SECRET_KEY
-		try:
-			checkout_session = stripe.checkout.Session.create(
-				success_url = domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-				cancel_url = domain_url + 'cancelled/',
-				payment_method_type = ['card'],
-				mode = 'payment',
-				line_items = [
-					{
-						'name':'T-shirt',
-						'quantity':1,
-						'currency':'jpy',
-						'amount':'200000',
-					}
-				]
-			)
-			return JsonResponse({'sessionId':checkout_session['id']})
-		except Exception as e:
-			return JsonResponse({'error':str(e)})
+    if request.method == 'GET':
+        domain_url = 'http://localhost:8000/'
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        try:
+            # Create new Checkout Session for the order
+            # Other optional params include:
+            # [billing_address_collection] - to display billing address details on the page
+            # [customer] - if you have an existing Stripe Customer ID
+            # [payment_intent_data] - lets capture the payment later
+            # [customer_email] - lets you prefill the email input in the form
+            # For full details see https:#stripe.com/docs/api/checkout/sessions/create
+
+            # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+            checkout_session = stripe.checkout.Session.create(
+                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url + 'cancelled/',
+                payment_method_types=['card'],
+                mode='payment',
+                line_items=[
+                    {
+                        'name': 'T-shirt',
+                        'quantity': 1,
+                        'currency': 'usd',
+                        'amount': '2000',
+                    }
+                ]
+            )
+            return JsonResponse({'sessionId': checkout_session['id']})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
